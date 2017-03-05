373b29224eb090e18d4cee7f61157bbb725ca317
Conor <ckosidowski11@winona.edu>
2017-03-05 14:50:34 -0600

    Adding the dummy login page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Gro" });
+router.get("/", function (req, res, next) {
+    res.render("index", {title: "Gro"});
+}).get("/login", function (req, res, next) {
+    res.render("login", {title: "Login"});
 });
 
 module.exports = router;
