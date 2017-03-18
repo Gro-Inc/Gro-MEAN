8808e1d5cdddee92f533c6d13ccf509c5575d079
rsinha <rsinha@fastenal.com>
2017-03-18 12:26:36 +0530

    GRO-001 Linking MongoDB

--- public/javascripts/root.js
+++ public/javascripts/root.js
@@ -2,7 +2,11 @@ var app = angular.module("root", []);
 
 app.controller("registerController", function ($scope, $http) {
     $scope.register = function () {
-        $http({url: "/services/user/register", method: "POST", params: {username: $scope.regUsername, password: $scope.regPassword}}).then(
+        $http({
+            url: "/user/register",
+            method: "POST",
+            params: {username: $scope.regUsername, password: $scope.regPassword}
+        }).then(
             function (response) {
             });
     };
@@ -10,17 +14,22 @@ app.controller("registerController", function ($scope, $http) {
 
 app.controller("chatController", function ($scope, $http) {
     $scope.messages = [];
-
-    $http({url: "/services/chat/get-messages"}).then(function (response) {
-        angular.forEach(response.data.data, function (message) {
-            $scope.messages.push(message);
+    $http({url: "/chat/get-messages"}).then(function (response) {
+        angular.forEach(response.data, function (messageArray) {
+            $scope.messages.push(messageArray.message);
+            $(".chat-box-middle").animate({scrollTop: $('.chat-box-middle').prop("scrollHeight")}, 1000);
         });
     });
 
     $scope.sendMessage = function (keyEvent) {
         if (keyEvent.which === 13 && $scope.chatMessage !== "") {
-            $http({url: "/services/chat/send-message", method: "POST", params: {message: $scope.chatMessage}}).then(function (response) {
-                $scope.messages.push(response.data.data);
+            $http({
+                url: "/chat/send-message",
+                method: "POST",
+                params: {message: $scope.chatMessage}
+            }).then(function (response) {
+                $scope.messages.push(response.data);
+                $(".chat-box-middle").animate({scrollTop: $('.chat-box-middle').prop("scrollHeight")}, 1000);
             });
 
             $scope.chatMessage = "";
